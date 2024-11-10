ffe0a55c0d7aebfb2d0c248eb4844c5803cd4089
Omar Anwar <anwrmr649@gmail.com>
2024-11-10 18:36:45 +0200

    moved files to git

---
+++ Al-nokhba/script.js
@@ -0,0 +1,26 @@
+let buttons = document.querySelectorAll("[data-carousel-button]");
+let carousel = document.querySelector(".carousel")
+let carouselImages = document.querySelector(".carousel-images");
+let carouselImagesChildren = carouselImages.children;
+function changeImage(change) {
+    
+    let activeSlide = carouselImages.querySelector("[data-active]");
+    let offsetIndex = [...carouselImagesChildren].indexOf(activeSlide) + change;
+    
+    if (offsetIndex > carouselImagesChildren.length - 1){
+        offsetIndex = 0;
+    }   
+    if (offsetIndex < 0){
+        offsetIndex = carouselImagesChildren.length - 1;    
+    } 
+    
+    carouselImagesChildren[offsetIndex].dataset.active = true;
+    carousel.style.background = carouselImagesChildren[offsetIndex].dataset.color;
+    delete activeSlide.dataset.active;
+}
+buttons.forEach( (button) => {
+    button.addEventListener("click", () => {
+        let change = button.dataset.carouselButton === "prev" ? 1 : -1;
+        changeImage(change);
+    })
+})
